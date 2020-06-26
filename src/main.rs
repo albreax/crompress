67e51c76e919063757bbb0281e5094b7c5346a99
Albrecht Naumann <albrecht.naumann@posteo.de>
2020-06-26 19:32:26 +0200

    implement cli file parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use structopt::StructOpt;
+use std::path::Path;
+
+#[derive(Debug)]
+#[derive(StructOpt)]
+struct Cli {
+    /// The path to the file to compress
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let args = Cli::from_args();
+    let path = Path::new(args.path.as_path());
+    println!("path: {:?} exits: {:?}!", path, path.exists());
+    
+    // TODO: read source file
+    // TODO: compress 
+    // TODO: write compressed file 
 }
