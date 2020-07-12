ea5afacbd064545b28fcb21c44281a8fdd32e2b7
Albrecht Naumann <albrecht.naumann@posteo.de>
2020-07-12 21:22:23 +0200

    read file to buffer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use structopt::StructOpt;
-use std::path::Path;
-
-#[derive(Debug)]
-#[derive(StructOpt)]
+use std::{io::{Read, Result}, path::Path, fs::File};
+#[derive(Debug, StructOpt)]
 struct Cli {
     /// The path to the file to compress
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+}
+
+fn open_file(path: &Path) -> File {
+    let f = File::open(path);
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!(format!("can't load file {}", error)),
+    };
+    f
+}
+
+fn buffer_file(mut file: &File) -> Result<Vec<u8>>{
+    let mut tmp: Vec<u8> = Vec::new();
+    file.read_to_end(&mut tmp)?;
+    Ok(tmp)
 }
 
 fn main() {
@@ -14,8 +27,15 @@ fn main() {
     let args = Cli::from_args();
     let path = Path::new(args.path.as_path());
     println!("path: {:?} exits: {:?}!", path, path.exists());
-    
+    let file: File = open_file(path);
+    let res = buffer_file(&file);
+    let buf = match res {
+        Ok(a) => a,
+        _ => unreachable!(),
+    };
+    print!("buf {:?}", buf);
+
     // TODO: read source file
-    // TODO: compress 
+    // TODO: compress or decompress
     // TODO: write compressed file 
 }
